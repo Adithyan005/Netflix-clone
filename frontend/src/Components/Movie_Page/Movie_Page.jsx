369052543c9b287eb9dadb1577fbb69cec1953f0
Adithyan <adithyanm.22cse@kongu.edu>
2025-01-07 22:54:31 +0530

    movie-page

--- frontend/src/Components/Movie_Page/Movie_Page.jsx
+++ frontend/src/Components/Movie_Page/Movie_Page.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import logo from "../../assets/logo.png";
+import star from "../../assets/star.png";
+import wstar from "../../assets/white-star.png";
 
 const Movie_Page = () => {
   const location = useLocation();
@@ -27,8 +29,33 @@ const Movie_Page = () => {
           </ul>
         </div>
       </div>
+      <div className="flex justify-between items-center gap-10 mt-5 ms-4 me-4 px-6">
+        <div className="font-stylish">
+          <h1 className="text-2xl font-semibold">{movieData.name}</h1>
+          <div className="flex gap-4 text-sm">
+            <p>{movieData.year}</p>
+            <p>{movieData.duration}</p>
+          </div>
+        </div>
+        <div className="flex gap-5 font-robo font-semibold">
+          <div>
+            <h1>IMDb Rating</h1>
+            <div className="flex gap-1 justify-center items-center text-md">
+              <img src={star} alt="" className="w-6" />
+              <p>{movieData.rating}/10</p>
+            </div>
+          </div>
+          <div>
+            <h1>Your Rating</h1>
+            <div className="flex gap-1 items-center justify-center text-md">
+              <img src={wstar} alt="" className="w-5" />
+              <p>Rate</p>
+            </div>
+          </div>
+        </div>
+      </div>
       <div>
-        <img src={movieData.image} alt="" />
+        <img src={movieData.image} alt="" className="w-[50vw]"/>
         <h1>{movieData.name}</h1>
       </div>
     </div>
