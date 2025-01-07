4a9efbe256bc52378214a18484498bbc7c3755bf
Adithyan <adithyanm.22cse@kongu.edu>
2025-01-07 21:07:47 +0530

    todays changes

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 import express from "express"
 import mongoose from "mongoose"
 import Movie_Model from "./models/Movie.js"
+import dotenv from "dotenv"
 
 const app=express()
 app.use(express.json())
 
-mongoose.connect("mongodb+srv://adithyanm22cse:YFMUMJIpsCJrbXP2@cluster0.kwm0w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+dotenv.config()
+const mongourl="mongodb+srv://adithyanm22cse:YFMUMJIpsCJrbXP2@cluster0.kwm0w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const port=process.env.port || 4000
+const secretkey=process.env.adi123
+
+mongoose.connect(mongourl)
 try {
     console.log('connected to database')
 } catch (error) {
@@ -32,8 +38,19 @@ app.post('/moviepage/insert',async(req,res)=>{
         console.log(error)
         res.status(500).json({"message":"Not inserted successfully"})
     }
-
 })
 
-app.listen(4000);
-console.log('server running on 4000')
+app.get('/movies',async(req,res)=>{
+    try {
+        const movie=await Movie_Model.find()
+        res.status(200).json(movie)
+        console.log(movie)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+app.listen(port,()=>{
+    console.log(`server running on port ${port}`)
+})
