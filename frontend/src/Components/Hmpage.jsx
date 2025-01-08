75f259d00f3a90cad6687a2a21d462c7d964f483
Adithyan <adithyanm.22cse@kongu.edu>
2025-01-08 23:43:50 +0530

    todays changes

--- frontend/src/Components/Hmpage.jsx
+++ frontend/src/Components/Hmpage.jsx
@@ -1,32 +1,33 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Hmpage = () => {
-    const [home,setHome]=useState([])
-    useEffect(()=>{
-        async function homedata() {
-            try {
-                const response = await axios.get('http://localhost:4000/homemovie')
-                setHome(response.data)
-            } catch (error) {
-                console.log('error fetching data')
-            }
-        }
-        homedata();
-    })
+  const [home, setHome] = useState([]);
+  useEffect(() => {
+    async function homedata() {
+      try {
+        const response = await axios.get("http://localhost:4000/homemovie");
+        setHome(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    homedata();
+  });
 
   return (
     <div>
-      {home.map((data,index)=>{
-        return(
-            <div key={index}>
-                <img src={data.bgimage} alt="" />
-            </div>
-        )
-      }
-
-      )}
+      {home.map((data, index) => {
+        return (
+          <div key={index} className="text-white w-[90vw] h-[100vh]">
+            <img src={data.bgimage} alt="" />
+            <img src={data.titleimage} alt="" />
+            <p>{data.description}</p>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Hmpage
+export default Hmpage;
