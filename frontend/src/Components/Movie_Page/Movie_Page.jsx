91086789d1c57e72f73b2e10570c7ce0a1be950a
Adithyan <adithyanm.22cse@kongu.edu>
2025-02-04 22:34:30 +0530

    responsive

--- frontend/src/Components/Movie_Page/Movie_Page.jsx
+++ frontend/src/Components/Movie_Page/Movie_Page.jsx
@@ -11,7 +11,9 @@ const Movie_Page = () => {
   const convertToEmbedUrl = (url) => {
     if (!url) return "";
     if (url.includes("watch?v=")) {
-      return `https://www.youtube.com/embed/${url.split("watch?v=")[1].split("&")[0]}`;
+      return `https://www.youtube.com/embed/${
+        url.split("watch?v=")[1].split("&")[0]
+      }`;
     } else if (url.includes("youtu.be/")) {
       return `https://www.youtube.com/embed/${url.split("youtu.be/")[1]}`;
     }
@@ -20,86 +22,83 @@ const Movie_Page = () => {
 
   const trailerUrl = convertToEmbedUrl(movieData.trailerurl);
 
-  console.log("movie page", movieData);
   return (
     <div className="text-white">
-      <div className="flex justify-between items-center">
-        <div className="ms-4 px-6 py-4">
-          <img src={logo} alt="" className="w-[6rem]" />
-        </div>
-        <div>
-          <ul className="flex gap-4 px-6 me-4 py-4 font-robo font-semibold text-lg">
-            <li>
-              <a href="#">Cast & Crew</a>
-            </li>
-            <li>
-              <a href="#">User Reviews</a>
-            </li>
-            <li>
-              <a href="#">FAQ</a>
-            </li>
-          </ul>
-        </div>
+      {/* Header */}
+      <div className="flex justify-between items-center px-3 py-4 md:px-6 md:py-4">
+        <img src={logo} alt="Logo" className="w-[25vw] md:w-[6rem]" />
+        <ul className="flex gap-3 text-sm font-semibold font-robo md:gap-4 md:text-lg">
+          <li><a href="#">Cast & Crew</a></li>
+          <li><a href="#">User Reviews</a></li>
+          <li><a href="#">FAQ</a></li>
+        </ul>
       </div>
-      <div className="flex justify-between items-center gap-10 mt-5 ms-4 me-4 px-6">
+
+      {/* Movie Info */}
+      <div className="flex justify-between gap-3 mt-3 px-3 md:flex-row md:justify-between md:items-center md:gap-10 md:mt-5 md:px-6">
         <div className="font-stylish">
-          <h1 className="text-2xl font-semibold">{movieData.name}</h1>
-          <div className="flex gap-4 text-sm">
+          <h1 className="text-lg font-semibold md:text-2xl">{movieData.name}</h1>
+          <div className="flex gap-2 text-sm md:gap-4">
             <p>{movieData.year}</p>
             <p>{movieData.duration}</p>
           </div>
         </div>
-        <div className="flex gap-5 font-robo font-semibold">
+        <div className="flex justify-between gap-5 font-robo font-semibold md:gap-5">
           <div>
             <h1>IMDb Rating</h1>
-            <div className="flex gap-1 justify-center items-center text-md">
-              <img src={star} alt="" className="w-5" />
+            <div className="flex gap-1 items-center">
+              <img src={star} alt="Star" className="w-5" />
               <p>{movieData.rating}/10</p>
             </div>
           </div>
           <div>
             <h1>Your Rating</h1>
-            <div className="flex gap-1 items-center justify-center text-md">
-              <img src={wstar} alt="" className="w-5" />
+            <div className="flex gap-2 items-center">
+              <img src={wstar} alt="White Star" className="w-6" />
               <p>Rate</p>
             </div>
           </div>
         </div>
       </div>
-      <div className="flex justify-start items-center gap-3 mt-6 ms-4 px-6">
-        <div>
-          <img src={movieData.image} alt="" className="w-[35vw]" />
-        </div>
-        <div className="me-4 px-6">
+
+      {/* Image & Trailer */}
+      <div className="flex flex-col gap-5 mt-5 px-5 md:flex-row md:justify-between md:items-center md:gap-4 md:mt-6 md:px-6">
+        <img src={movieData.image} alt="Movie" className="w-[90vw] md:w-[35vw]" />
+        <div className="px-4 md:px-6">
           {trailerUrl ? (
             <iframe
-              width="770"
-              height="380"
+              width="100%"
+              height="200"
               src={trailerUrl}
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
+              className="md:w-[770px] md:h-[380px]"
             ></iframe>
           ) : (
             <p className="text-red-500">Trailer not available</p>
           )}
         </div>
       </div>
-      <div className="font-stylish text-justify">
-        <div className="mt-7 px-6 ms-4 w-[70vw] text-justify">
-          <p className="tracking-wide">{movieData.description}</p>
-        </div>
-        <div className="flex gap-8 mt-7 px-6 ms-4 items-center">
-          <h1 className="font-semibold text-xl">Director</h1>
+
+      {/* Movie Description */}
+      <div className="font-stylish text-justify mt-7 px-3 md:px-6 md:w-[70vw]">
+        <p className="tracking-wide">{movieData.description}</p>
+      </div>
+
+      {/* Movie Details */}
+      <div className="flex flex-col gap-5 mt-7 mb-6 px-3 text-lg md:flex-col md:gap-8 md:px-6">
+        <div className="flex items-center gap-4 text-lg ">
+          <h1 className="font-semibold text-lg md:text-xl">Director</h1>
           <p className="text-cyan-500">{movieData.director}</p>
         </div>
-        <div className="flex gap-9 mt-7 px-6 ms-4 items-center">
-          <h1 className="font-semibold text-xl">Writers</h1>
+        <div className="flex items-center gap-5">
+          <h1 className="font-semibold text-lg md:text-xl">Writers</h1>
           <p className="text-cyan-500">{movieData.writers}</p>
         </div>
-        <div className="flex gap-14 mt-7 mb-7 px-6 ms-4 items-center">
-          <h1 className="font-semibold text-xl">Stars</h1>
+        <div className="flex items-center gap-9">
+          <h1 className="font-semibold text-lg md:text-xl">Stars</h1>
           <p className="text-cyan-500">{movieData.cast}</p>
         </div>
       </div>
--- frontend/src/Components/Signup/Signup.jsx
+++ frontend/src/Components/Signup/Signup.jsx
@@ -40,37 +40,37 @@ const Signup = () => {
 
   return (
       <div className="text-white font-stylish">
-        <div className="p-6" style={bgstyle}>
-          <img src={logo} alt="" className="w-[10vw]" />
-          <div className="bg-black bg-opacity-70 max-w-[27vw] mx-auto max-h-[100vh] px-5 mt-5">
+        <div className="p-6 md:p-6" style={bgstyle}>
+          <img src={logo} alt="" className="w-[27vw] md:w-[10vw]" />
+          <div className="bg-black bg-opacity-70 max-w-[80vw] mx-auto max-h-[80vh] px-3 mt-20 pb-4 md:bg-black md:bg-opacity-70 md:max-w-[27vw] md:mx-auto md:max-h-[120vh] md:px-5 md:mt-5 md:pb-4">
             <form onSubmit={handlesubmit}>
-            <div className="pt-14 ms-6 ">
-              <h1 className="text-3xl font-semibold">Sign Up</h1>
+            <div className="pt-10 ms-6 md:pt-14 md:ms-6 ">
+              <h1 className="text-2xl font-semibold md:text-3xl md:font-semibold">Sign Up</h1>
             </div>
-            <div className="flex flex-col justify-center items-center ">
-              <div className="pt-5">
+            <div className="flex flex-col justify-center items-center md:flex md:flex-col md:justify-center md:items-center">
+              <div className="pt-5 md:pt-5">
                 <input
                   type="text"
                   placeholder="Email"
                   name="email" value={email} onChange={(e)=>setEmail(e.target.value)}
-                  className="bg-gray-600 w-[20vw] h-10 p-5"
+                  className="bg-gray-600 w-[60vw] h-10 p-5 md:bg-gray-600 md:w-[20vw] md:h-10 md:p-5"
                 />
               </div>
-              <div className="pt-5">
+              <div className="pt-5 md:pt-5">
                 <input
                   type="password"
                   placeholder="Password"
                   name="password" value={password} onChange={(e)=>setPassword(e.target.value)}
-                  className="bg-gray-600  w-[20vw] h-10 p-5"
+                  className="bg-gray-600 w-[60vw] h-10 p-5 md:bg-gray-600 md:w-[20vw] md:h-10 md:p-5"
                 />
               </div>
-              <div className="pt-7">
-                <button className="bg-red-700 w-[20vw] h-10">Sign Up</button>
+              <div className="pt-8 md:pt-7">
+                <button className="bg-red-700 w-[60vw] h-10 md:bg-red-700 md:w-[20vw] md:h-10">Sign Up</button>
                 <ToastContainer/>
               </div>
             </div>
-            <div className="ms-6 pt-1.5 flex max-w-[20vw] justify-between items-center">
-              <div className="flex gap-3">
+            <div className="ms-6 pt-2 flex max-w-[60vw] justify-between items-center text-sm md:ms-6 md:pt-1.5 md:flex md:max-w-[20vw] md:justify-between md:items-center">
+              <div className="flex gap-2 md:flex md:gap-3">
                 <input type="checkbox" />
                 <h1>Remember Me</h1>
               </div>
@@ -78,7 +78,7 @@ const Signup = () => {
                 <h1>Need Help?</h1>
               </div>
             </div>
-            <div className="ms-6 pt-14 flex gap-2 pb-14">
+            <div className="ms-6 pt-7 flex gap-7 text-sm pb-3 md:ms-6 md:pt-10 md:flex md:gap-2 md:pb-3">
               <h1>Already a User?</h1>
               <a href="/"><h1>Login</h1></a>
             </div>
