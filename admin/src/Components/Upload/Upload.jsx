a8b650d45d2f93632dfc997045704ab7fb36d40e
Adithyan <adithyanm.22cse@kongu.edu>
2025-06-07 17:08:22 +0530

    form creation

--- admin/src/Components/Upload/Upload.jsx
+++ admin/src/Components/Upload/Upload.jsx
@@ -1,43 +1,133 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const Upload = () => {
+  const [type, setType] = useState("");
+  const [image, setImage] = useState("");
+  const [name, setName] = useState("");
+  const [year, setYear] = useState("");
+  const [duration, setDuration] = useState("");
+  const [director, setDirector] = useState("");
+  const [cast, setCast] = useState("");
+  const [writers, setWriters] = useState("");
+  const [rating, setRating] = useState("");
+  const [description, setDescription] = useState("");
+  const [trailerurl, setTrailerurl] = useState("");
+
+  const fileref = useRef(null);
+
+  const handlesubmit=()=>{
+    const file=fileref.current?.values[0];
+    console.log(file)
+  }
+
   return (
     <div>
       <div className="flex justify-center items-center p-4 font-medium">
         <h1 className="text-2xl font-stylish">Upload Movies</h1>
       </div>
       <div>
-        <form action="">
+        <form onSubmit={handlesubmit}>
           <div className="bg-yellow-100">
             <div>
-              <input type="text" placeholder="image" />
+              <label htmlFor="">
+                <input
+                  type="radio"
+                  name=""
+                  value="opt1"
+                  checked={type === "opt1"}
+                  onChange={(e) => setType(e.target.value)}
+                />
+                Blockbuster
+              </label>
+              <label htmlFor="">
+                <input
+                  type="radio"
+                  name=""
+                  value="opt2"
+                  checked={type === "opt2"}
+                  onChange={(e) => setType(e.target.value)}
+                />
+                Only on Netflix
+              </label>
             </div>
             <div>
-              <input type="text" placeholder="name" />
+              <input
+                type="file"
+                placeholder="image"
+                ref={fileref}
+              />
             </div>
             <div>
-              <input type="text" placeholder="year" />
+              <input
+                type="text"
+                placeholder="name"
+                value="name"
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
             <div>
-              <input type="text" placeholder="duration" />
+              <input
+                type="text"
+                placeholder="year"
+                value="year"
+                onChange={(e) => setYear(e.target.value)}
+              />
             </div>
             <div>
-              <input type="text" placeholder="director" />
+              <input
+                type="text"
+                placeholder="duration"
+                value="duration"
+                onChange={(e) => setDuration(e.target.value)}
+              />
             </div>
             <div>
-              <input type="text" placeholder="cast" />
+              <input
+                type="text"
+                placeholder="director"
+                value="director"
+                onChange={(e) => setDirector(e.target.value)}
+              />
             </div>
             <div>
-              <input type="text" placeholder="writers" />
+              <input
+                type="text"
+                placeholder="cast"
+                value="cast"
+                onChange={(e) => setCast(e.target.value)}
+              />
             </div>
             <div>
-              <input type="text" placeholder="rating" />
+              <input
+                type="text"
+                placeholder="writers"
+                value="writers"
+                onChange={(e) => setWriters(e.target.value)}
+              />
             </div>
             <div>
-              <input type="text" placeholder="description" />
+              <input
+                type="text"
+                placeholder="rating"
+                value="rating"
+                onChange={(e) => setRating(e.target.value)}
+              />
             </div>
             <div>
-              <input type="text" placeholder="trailerurl" />
+              <input
+                type="text"
+                placeholder="description"
+                value="description"
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="trailerurl"
+                value="trailerurl"
+                onChange={(e) => setTrailerurl(e.target.value)}
+              />
             </div>
           </div>
         </form>
